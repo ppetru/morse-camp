760381af33243f8f1bc19acab929d7e1e45a0e6b
Petru Paler <petru@paler.net>
2017-10-05 17:55:42 +0200

    Install react-md.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
