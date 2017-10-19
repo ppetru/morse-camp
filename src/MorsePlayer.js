0cbf5b267a93e1d9d17568bff7f6342de003f3c7
Petru Paler <petru@paler.net>
2017-10-19 19:38:04 +0200

    Removed some unused features from MorsePlayer and make it take the audio context via props.

--- src/MorsePlayer.js
+++ src/MorsePlayer.js
@@ -4,116 +4,7 @@ import PropTypes from 'prop-types';
 
 class MorsePlayer extends Component {
 
-  speed = (wpm) => {
-		// 1200ms per dit is 1WPM
-		this.ditLength = 1200/wpm;
-	}
-
-  play = (message) => {
-		// Use already saved message if no new message is given
-		message = message || this.message;
-
-		// nothing to do?
-		if (message === '') {
-			for (let i=0;i<this.messageCallbacks.length;i++)
-				this.messageCallbacks[i]();
-			return;
-		}
-
-		// already playing?
-		if (this.playing) return;
-		this.playing = true;
-
-		// Fetch next character from message string:
-		var character = message[0];
-		this.message = message.slice(1);
-		// TODO respect <KA> <SOS>
-
-		// Lookup morse code for next character
-		if (!this.morseCodes[character]) {
-			console.log("Character '"+character+"' unknown.");
-			character = " ";
-		}
-		// Send next character as plain text back to visualisation or anything:
-		for (let i=0;i<this.characterCallbacks.length;i++)
-			this.characterCallbacks[i](character);
-		character = this.morseCodes[character];
-
-
-		var lengthSum=0;
-    console.log(this.context);
-    console.log(this.context.currentTime);
-    console.log(this.gainNode);
-		var now = this.context.currentTime;
-		// set ramp time to 2 periods
-		var rampTime = 1/this.oscillatorNode.frequency.value*2;
-		for(let i=0;i<character.length;i++) {
-			// iterate dits and dahs
-			if (character[i]==='·') {
-				console.log(now, (lengthSum+1), this.ditLength/1000);
-				this.gainNode.gain.linearRampToValueAtTime(
-					0, now + (lengthSum+1)*this.ditLength/1000);
-				this.gainNode.gain.linearRampToValueAtTime(
-					this.volume, now + (lengthSum+1)*this.ditLength/1000 +
-					rampTime);
-
-				this.gainNode.gain.linearRampToValueAtTime(
-					this.volume, now + (lengthSum+2)*this.ditLength/1000);
-				this.gainNode.gain.linearRampToValueAtTime(
-					0, now + (lengthSum+2)*this.ditLength/1000 +
-					rampTime);
-
-				lengthSum+=2;
-			} else if (character[i]==='−') {
-				this.gainNode.gain.linearRampToValueAtTime(
-					0, now + (lengthSum+1)*this.ditLength/1000);
-				this.gainNode.gain.linearRampToValueAtTime(
-					this.volume, now + (lengthSum+1)*this.ditLength/1000 +
-					rampTime);
-
-				this.gainNode.gain.linearRampToValueAtTime(
-					this.volume, now + (lengthSum+4)*this.ditLength/1000);
-				this.gainNode.gain.linearRampToValueAtTime(
-					0, now + (lengthSum+4)*this.ditLength/1000 +
-					rampTime);
-
-				lengthSum+=4;
-			} else if (character[i]===' ') {
-				lengthSum+=3; // plus 2 for last character and 2 for this added below = 7
-			}
-		}
-    window.setTimeout(() => {
-			this.playing = false;
-			this.play();
-		}, (lengthSum+2+this.farnsworth)*this.ditLength);
-		// two additional dits pause sum up to three dits between characters
-
-	} // play
-
-  componentDidMount() {
-
-    this.speed(this.props.speed);
-
-    // Volume 1 = 100%, more leads to harmonics due to overmodulation
-    this.volume = 1;
-
-    // extra pause after every character (dit-length)
-    // default = 0, use 2 or 3 for training
-    this.farnsworth = 0;
-
-    // methods to call at beginning of each character
-    this.characterCallbacks = [];
-
-    // methods to call at successful end of playing message
-    this.messageCallbacks = [];
-
-    // already playing a message?
-    this.playing = false;
-
-    // Message to play next
-    this.message = '';
-
-    this.morseCodes = {
+  morseCodes = {
       // third column: greek
       // fourth column: cyrillic
       'A':'·−',   'a':'·−',   'Α':'·−',   'А':'·−',
@@ -205,15 +96,104 @@ class MorsePlayer extends Component {
       '>':'    ', // "visible" pause before message
       '\t':'  ',
       '\n':'   ',
-    }
+  }
 
-    this.context = new (window.AudioContext || window.webkitAudioContext)();
+  speed = (wpm) => {
+		// 1200ms per dit is 1WPM
+		this.ditLength = 1200/wpm;
+	}
 
-    this.gainNode = this.context.createGain();
-    this.gainNode.connect(this.context.destination);
+  play = (message) => {
+		// Use already saved message if no new message is given
+		message = message || this.message;
+    console.log(message);
+
+		// nothing to do?
+		if (message === '') {
+			return;
+		}
+
+		// already playing?
+		if (this.playing) return;
+		this.playing = true;
+
+		// Fetch next character from message string:
+		var character = message[0];
+		this.message = message.slice(1);
+		// TODO respect <KA> <SOS>
+
+		// Lookup morse code for next character
+		if (!this.morseCodes[character]) {
+			console.log("Character '"+character+"' unknown.");
+			character = " ";
+		}
+		character = this.morseCodes[character];
+
+
+		var lengthSum=0;
+		var now = this.props.audioContext.currentTime;
+		// set ramp time to 2 periods
+		var rampTime = 1/this.oscillatorNode.frequency.value*2;
+		for(let i=0;i<character.length;i++) {
+			// iterate dits and dahs
+			if (character[i]==='·') {
+				this.gainNode.gain.linearRampToValueAtTime(
+					0, now + (lengthSum+1)*this.ditLength/1000);
+				this.gainNode.gain.linearRampToValueAtTime(
+					this.volume, now + (lengthSum+1)*this.ditLength/1000 +
+					rampTime);
+
+				this.gainNode.gain.linearRampToValueAtTime(
+					this.volume, now + (lengthSum+2)*this.ditLength/1000);
+				this.gainNode.gain.linearRampToValueAtTime(
+					0, now + (lengthSum+2)*this.ditLength/1000 +
+					rampTime);
+
+				lengthSum+=2;
+			} else if (character[i]==='−') {
+				this.gainNode.gain.linearRampToValueAtTime(
+					0, now + (lengthSum+1)*this.ditLength/1000);
+				this.gainNode.gain.linearRampToValueAtTime(
+					this.volume, now + (lengthSum+1)*this.ditLength/1000 +
+					rampTime);
+
+				this.gainNode.gain.linearRampToValueAtTime(
+					this.volume, now + (lengthSum+4)*this.ditLength/1000);
+				this.gainNode.gain.linearRampToValueAtTime(
+					0, now + (lengthSum+4)*this.ditLength/1000 +
+					rampTime);
+
+				lengthSum+=4;
+			} else if (character[i]===' ') {
+				lengthSum+=3; // plus 2 for last character and 2 for this added below = 7
+			}
+		}
+    window.setTimeout(() => {
+			this.playing = false;
+			this.play();
+		}, (lengthSum+2)*this.ditLength);
+		// two additional dits pause sum up to three dits between characters
+
+	} // play
+
+  componentDidMount() {
+
+    this.speed(this.props.speed);
+
+    // Volume 1 = 100%, more leads to harmonics due to overmodulation
+    this.volume = 1;
+
+    // already playing a message?
+    this.playing = false;
+
+    // Message to play next
+    this.message = '';
+
+    this.gainNode = this.props.audioContext.createGain();
+    this.gainNode.connect(this.props.audioContext.destination);
     this.gainNode.gain.value = 0;
 
-    this.oscillatorNode = this.context.createOscillator();
+    this.oscillatorNode = this.props.audioContext.createOscillator();
     this.oscillatorNode.frequency.value = this.props.frequency;
     this.oscillatorNode.connect(this.gainNode);
     this.oscillatorNode.start(0);
@@ -245,6 +225,7 @@ MorsePlayer.propTypes = {
   speed: PropTypes.number.isRequired,
   frequency: PropTypes.number.isRequired,
   message: PropTypes.string.isRequired,
+  audioContext: PropTypes.object.isRequired,
 }
 
 export default MorsePlayer;
