0e2580942cd0688a69510f6628996037a0060d7c
Petru Paler <petru@paler.net>
2017-10-19 19:40:46 +0200

    Snapshot sketch of state flow.

--- src/Worder.js
+++ src/Worder.js
@@ -1,21 +1,106 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import Button from 'react-md/lib/Buttons/Button';
-import SelectField from 'react-md/lib/SelectFields';
-import TextField from 'react-md/lib/TextFields';
+import {
+  Button,
+  Paper
+} from 'react-md';
 
 import MorsePlayer from './MorsePlayer';
 
-class Worder extends Component {
+class StartStep extends Component {
+  render() {
+    return (
+      <Button
+        raised
+        primary
+        onClick={this.props.onStateChange}
+      >
+        Start
+      </Button>
+    )
+  }
+}
+StartStep.propTypes = {
+  onStateChange: PropTypes.func.isRequired,
+}
+
+class PlayStep extends Component {
+  pickWord() {
+    return "hi";
+  }
+
+  componentDidMount() {
+    const word = this.pickWord();
+    this.setState({ word })
+    this.props.setMorse(word);
+  }
+
   render() {
     return (
       <div>
+        <Button
+          raised
+          primary
+          onClick={this.props.onStateChange}
+        >
+          Show
+        </Button>
+        <Button
+          raised
+          primary
+          onClick={this.props.onStateChange}
+        >
+          Show
+        </Button>
+        <Button
+          raised
+          primary
+          onClick={this.props.onStateChange}
+        >
+          Stop
+        </Button>
+      </div>
+    )
+  }
+}
+PlayStep.propTypes = {
+  onStateChange: PropTypes.func.isRequired,
+  setMorse: PropTypes.func.isRequired,
+}
+
+class Worder extends Component {
+  state = {
+    morseText: "",
+    flowStep: "start",
+  };
+
+  render() {
+    var step;
+    switch (this.state.flowStep) {
+        case "start":
+          step = <StartStep onStateChange={() => this.setState({flowStep: "play"})} />
+          break;
+        case "play":
+          step = <PlayStep
+                  onStateChange={(step) => this.setState({flowStep: step})}
+                  setMorse={(text) => this.setState({morseText: text})}
+                />
+          break;
+        default:
+    }
+    return (
+      <Paper zDepth={2} className="md-grid">
         <MorsePlayer
           speed={30}
           frequency={500}
-          message="hi"
+          message={this.state.morseText}
+          audioContext={new (window.AudioContext || window.webkitAudioContext)()}
         />
-      </div>
+        <div className="">
+          {step}
+        </div>
+      </Paper>
     )
   }
 }
