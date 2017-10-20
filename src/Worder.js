5a03c8aaae317d72704b6463ec41cecd9602f79d
Petru Paler <petru@paler.net>
2017-10-20 22:58:39 +0200

    Repeat playing the chosed word until stopped or another word is picked.

--- src/Worder.js
+++ src/Worder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { autorun } from 'mobx';
 import { inject, observer } from 'mobx-react';
 
 import {
@@ -9,112 +10,139 @@ import {
 
 import MorsePlayer from './MorsePlayer';
 
-const StartStep = inject("store")(observer(class StartStep extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Get ready to listen</h1>
-        <Button
-          raised
-          primary
-          onClick={this.props.store.play}
-        >
-          Start
-        </Button>
-      </div>
-    )
-  }
-}))
-StartStep.propTypes = {
+const StartStep = inject("store")(observer(({ store }) =>
+  <div>
+    <h1>Get ready to listen</h1>
+    <Button
+      raised
+      primary
+      onClick={store.play}
+    >
+      Start
+    </Button>
+  </div>
+))
+
+const PlayingButtons = inject("store")(observer(({ store }) =>
+  <div>
+    <Button
+      raised
+      primary
+      onClick={store.show}
+    >
+      Show
+    </Button>
+    <Button
+      raised
+      primary
+      onClick={store.start}
+    >
+      Stop
+    </Button>
+  </div>
+))
+
+const PlayingStatus = inject("store")(observer(({ store }) => (
+  store.playing ? <h1>Playing...</h1>
+                : <h1>Waiting...</h1>
+)))
+
+const ShowStep = inject("store")(observer(({ store, word }) => (
+  <div>
+    <h1>The word is: {word}</h1>
+    <Button
+      raised
+      primary
+      onClick={store.play}
+    >
+      Correct
+    </Button>
+    <Button
+      raised
+      primary
+      onClick={store.play}
+    >
+      Incorrect
+    </Button>
+    <Button
+      raised
+      primary
+      onClick={store.start}
+    >
+      Stop
+    </Button>
+  </div>
+)))
+ShowStep.propTypes = {
+  word: PropTypes.string.isRequired,
 }
 
 const PlayStep = inject("store")(observer(class PlayStep extends Component {
-  pickWord() {
-    return "hi";
+  pickWord = () => {
+    return "e";
   }
 
   componentDidMount() {
-    this.props.store.playText(this.pickWord());
+    this.word = null;
+    this.timeout = null;
+    this.autorun = autorun(() => {
+      if (!this.props.store.playing) {
+        if (this.word === null) {
+          this.word = this.pickWord();
+          this.props.store.playText(this.word);
+        } else {
+          this.timeout = setTimeout(() => this.props.store.playText(this.word), 1000);
+        }
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.autorun();
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
   }
 
   render() {
-    return (
-      <div>
-        <h1>Playing...</h1>
-        <Button
-          raised
-          primary
-          onClick={this.props.store.show}
-        >
-          Show
-        </Button>
-        <Button
-          raised
-          primary
-          onClick={this.props.store.start}
-        >
-          Stop
-        </Button>
-      </div>
-    )
-  }
-}))
-PlayStep.propTypes = {
-}
-
-const ShowStep = inject("store")(observer(class ShowStep extends Component {
-  render() {
-    return (
-      <div>
-        <h1>The word was: {this.props.store.morseText}</h1>
-        <Button
-          raised
-          primary
-          onClick={this.props.store.play}
-        >
-          Correct
-        </Button>
-        <Button
-          raised
-          primary
-          onClick={this.props.store.play}
-        >
-          Incorrect
-        </Button>
-      </div>
-    )
-  }
-}))
-ShowStep.propTypes = {
-}
-
-const Worder = inject("store")(observer(class Worder extends Component {
-  render() {
-    var step;
-    switch (this.props.store.step) {
-        case "start":
-          step = <StartStep store={this.props.store} />
-          break;
-        case "play":
-          step = <PlayStep store={this.props.store} />
-          break;
-        case "show":
-          step = <ShowStep store={this.props.store} />
-          break;
-        default:
+    let content;
+    if (this.props.store.step === "play") {
+      content = <PlayingButtons />
+    } else {
+      content = <ShowStep word={this.word} />
     }
     return (
-      <Paper zDepth={2} className="md-grid">
-        <MorsePlayer
-          speed={30}
-          frequency={500}
-        />
-        <div className="">
-          {step}
-        </div>
-      </Paper>
+      <div>
+        <PlayingStatus />
+        {content}
+      </div>
     )
   }
+}))
+
+const Worder = inject("store")(observer(({ store }) => {
+  let step;
+  switch (store.step) {
+      case "start":
+        step = <StartStep />
+        break;
+      case "play":
+      case "show":
+        step = <PlayStep />
+        break;
+      default:
+  }
+  return (
+    <Paper zDepth={2} className="md-grid">
+      <MorsePlayer
+        speed={30}
+        frequency={500}
+      />
+      <div className="">
+        {step}
+      </div>
+    </Paper>
+  )
 }))
 
 export default Worder;
