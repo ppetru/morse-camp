440b8bd654573859cfb9773d145eedb0b55f0fd2
Petru Paler <petru@paler.net>
2017-11-16 21:54:05 +0100

    Add alphabet, digits, common ham punctuation and prosigns.

--- src/WordPicker.js
+++ src/WordPicker.js
@@ -5,6 +5,13 @@ import { words as top5k } from './words/top5k.js';
 function makeWordMap() {
   var map = [];
 
+  map[1] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+    '4', '5', '6', '7', '8', '9', '.', ',', '?', '<AR>', '<AS>', '<BK>',
+    '<CL>', '<KN>', '<SK>',
+  ];
+
   function registerWord(w) {
     const len = w.length;
     if (len in map) {
