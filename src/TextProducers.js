0f260028641db6e079bdd32bd1cec649dc55f58d
Petru Paler <petru@paler.net>
2017-12-08 18:29:28 +0100

    Fix another count limit bug.

--- src/TextProducers.js
+++ src/TextProducers.js
@@ -28,7 +28,7 @@ function withSizeLimit(limit, func) {
 
 function withCountLimit(name, limit, func) {
   return (size, pattern, total, index) => {
-    const count = pattern.slice(1).filter(p => p.startsWith(name)).length;
+    const count = pattern.filter(p => p.startsWith(name)).length;
     if (count < limit) {
       return func(size, pattern, total, index);
     } else {
