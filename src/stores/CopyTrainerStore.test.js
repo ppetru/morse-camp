6ed7404801de896aef40498a5624fa53962f03b7
Petru Paler <petru@paler.net>
2017-12-28 18:40:59 +0100

    Refactor tests.

--- src/stores/CopyTrainerStore.test.js
+++ src/stores/CopyTrainerStore.test.js
@@ -1,47 +1,64 @@
 import CopyTrainerStore from "./CopyTrainerStore";
 import FakeTransport from "../FakeTransport";
 
-it("deals with min > max", () => {
-  const store = new CopyTrainerStore(null, new FakeTransport());
-  store.setMaxLength(3);
-  store.setMinLength(5);
-  expect(store.maxLength).toEqual(5);
-});
+describe("CopyTrainerStore", () => {
+  let testStore;
+  let fakeTransport;
 
-it("deals with max < min", () => {
-  const store = new CopyTrainerStore(null, new FakeTransport());
-  store.setMinLength(5);
-  store.setMaxLength(3);
-  expect(store.minLength).toEqual(3);
-});
+  const store = () => {
+    if (!testStore) {
+      if (!fakeTransport) {
+        fakeTransport = new FakeTransport();
+      }
+      testStore = new CopyTrainerStore(
+        null,
+        fakeTransport,
+        true /* noDebounce */
+      );
+    }
+    return testStore;
+  };
 
-it("deals with invalid values", () => {
-  const store = new CopyTrainerStore(null, new FakeTransport());
-  store.setMinLength("foo");
-  expect(store.minLength).toEqual(0);
-  store.setMaxLength("bar");
-  expect(store.maxLength).toEqual(0);
-});
-
-it("saves and restores settings", () => {
-  const transport = new FakeTransport();
-  var store = new CopyTrainerStore(null, transport, true);
-  store.setMinLength(3);
-  store.setMaxLength(9);
-  store = new CopyTrainerStore(null, transport, true);
-  return store.loadSettings.then(() => {
-    expect(store.minLength).toEqual(3);
-    expect(store.maxLength).toEqual(9);
+  beforeEach(() => {
+    fakeTransport = undefined;
+    testStore = undefined;
   });
-});
 
-it("saves and restores word feedback", () => {
-  const transport = new FakeTransport();
-  var store = new CopyTrainerStore(null, transport, true);
-  store.textFeedback("foo bar", 1, 2, 0);
-  store = new CopyTrainerStore(null, transport, true);
-  return store.loadSettings.then(() => {
-    expect(store.words.keys()).toContain("foo");
-    expect(store.words.keys()).toContain("bar");
+  it("deals with min > max", () => {
+    store().setMaxLength(3);
+    store().setMinLength(5);
+    expect(store().maxLength).toEqual(5);
+  });
+
+  it("deals with max < min", () => {
+    store().setMinLength(5);
+    store().setMaxLength(3);
+    expect(store().minLength).toEqual(3);
+  });
+
+  it("deals with invalid values", () => {
+    store().setMinLength("foo");
+    expect(store().minLength).toEqual(0);
+    store().setMaxLength("bar");
+    expect(store().maxLength).toEqual(0);
+  });
+
+  it("saves and restores settings", () => {
+    store().setMinLength(3);
+    store().setMaxLength(9);
+    testStore = undefined;
+    return store().loadSettings.then(() => {
+      expect(store().minLength).toEqual(3);
+      expect(store().maxLength).toEqual(9);
+    });
+  });
+
+  it("saves and restores word feedback", () => {
+    store().textFeedback("foo bar", 1, 2, 0);
+    testStore = undefined;
+    return store().loadSettings.then(() => {
+      expect(store().words.keys()).toContain("foo");
+      expect(store().words.keys()).toContain("bar");
+    });
   });
 });
