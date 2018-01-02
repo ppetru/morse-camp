81fcec89c5f1275ce46c603a5f563ae0b4b0cf24
Petru Paler <petru@paler.net>
2018-01-02 21:07:57 +0100

    Fix broken test.

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
 import "web-audio-test-api";
+import { createMemoryHistory } from "history";
 
 import App from "./App";
 import MorsePlayer from "./MorsePlayer";
@@ -13,10 +14,12 @@ it("renders without crashing", () => {
   const store = new RootStore(transport);
   const audioContext = new AudioContext();
   const player = new MorsePlayer(store.morse, audioContext);
+  const history = createMemoryHistory();
   const div = document.createElement("div");
+
   ReactDOM.render(
     <Provider store={store} morsePlayer={player}>
-      <App />
+      <App history={history} />
     </Provider>,
     div
   );
