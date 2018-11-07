42435fa69aa61ec89468c0f282a1ae91ab53fc3e
Kurt Zoglmann <kaz0358@ksu.edu>
2018-11-07 09:30:16 -0600

    Minor. Corrected mispellings in dictionary.
    Also added TODO.

--- src/Words.js
+++ src/Words.js
@@ -5648,7 +5648,7 @@ const WORDS = [
   ["Nigeria", "n", 5000, "Country"],
   ["Norway", "n", 5000, "Country"],
   ["Oman", "n", 5000, "Country"],
-  ["Packistan", "n", 5000, "Country"],
+  ["Pakistan", "n", 5000, "Country"],
   ["Palau", "n", 5000, "Country"],
   ["Panama", "n", 5000, "Country"],
   ["Papua New Guinea", "n", 5000, "Country"],
@@ -5682,7 +5682,7 @@ const WORDS = [
   ["Sudan", "n", 5000, "Country"],
   ["Suriname", "n", 5000, "Country"],
   ["Swaziland", "n", 5000, "Country"],
-  ["Sweeden", "n", 5000, "Country"],
+  ["Sweden", "n", 5000, "Country"],
   ["Switzerland", "n", 5000, "Country"],
   ["Syria", "n", 5000, "Country"],
   ["Taiwan", "n", 5000, "Country"],
@@ -5758,7 +5758,7 @@ class Dictionary {
   setActiveWords(types) {
     this.minWordLength = Number.MAX_SAFE_INTEGER;
     this.maxWordLength = 0;
-    this.wordFrequency = new Map();
+    this.wordFrequency = new Map(); // TODO: this is used in sorted value order, consider using http://www.collectionsjs.com/sorted-map
     this.wordType = new Map();
 
     this.types = types;
